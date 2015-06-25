2784fef85f952cc0a2494fa20a55ada1592e60dd
PeJa Coding <3dokta@gmail.com>
2015-06-25 18:32:42 +0200

    added shared.sx support

--- MovieGateway.cs
+++ MovieGateway.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
 using System.Globalization;
 using System.Net.Http;
-using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,14 +11,12 @@ using Microsoft.Win32;
 using MovieGateway.Properties;
 using nUpdate.Updating;
 
-
 namespace MovieGateway
 {
 
 
     internal class MovieGateway : ApplicationContext
     {
-        //Component declarations
         private NotifyIcon _trayIcon;
         private ContextMenuStrip _trayIconContextMenu;
         private ToolStripMenuItem _closeMenuItem;
@@ -34,12 +29,12 @@ namespace MovieGateway
             Application.ApplicationExit += OnApplicationExit;
             ClipboardNotification.ClipboardUpdate += ClipboardNotification_ClipboardUpdate;
             _trayIcon.Visible = true;
-            
-        }
 
-        void manager_UpdateSearchStarted(object sender, EventArgs e)
-        {
-            _trayIcon.ShowBalloonTip(5,"Searching for Updates","...", ToolTipIcon.Info);
+            UpdateManager manager = new UpdateManager(new Uri("http://pejacoding.bplaced.net/MovieGateway/updates.json"), "<RSAKeyValue><Modulus>r0liu3a9sg3IbGxXIgSzuS8hbRiRk+4UsS4W6WqgrVJi2QHjKo8/9JzXCGgWHMXYxrtcWGQCHO3lUboSxLa3rDSU8fX/tuykPBqkOzeJln4pKqb84fXQLB3LLjr5KdjV98DrjuD/TAV292m1ZutpvWRqX4vstuCg9FuI7+C3CnnjVIorRg2fQumj6Z4az5n06KSsnO6BXqDhG1DDqUFBGw7n6eJmp5Mipuy88OXDvwna71NnG2BlVrlEeJBddPuh3vkypPSrgxhkQTgvMT+s4ujqCfsDIXdC5+UvWBua9um8RUDzGIg7TPmfUTUH/pc2pJZnK7FmLRd1kGa2Nfq+uyXLBsrJEYwJdCo4PEHRpD7ZNZUIxXx4AmUpH0+Ez7FpW7JPkVUSbB+lREQfzpFdGZ03qx/qicK92Wfq5NxyxOjL0F61n20p+6apemOGMNmIMarQv5vtK6FIofZwNpMSF8eDFOsb6utgur1BLFOvMIUSNJQXUKtNmoJbLjDw+UUoc4nAXZcjPr/9kPvCLs0SjxUp+9Bhze/0ju9WdQNcKRGFemOwWSzj9BwWNIbOlxE8iHort1Kr/Lm5vDRTPWjEBwBpRBK+T7OvEkkOJxQRsAKMeCcG5RaFjhjdn3OB7LsL9j8N+REvyLySXbQim9/mvLZhTnApDVDDSNX86Dg85A30AEnPCtz4szikxsOoPx2KFOImhjM94RlOeF9/rVLFTRYUW4wSSABVv+U0+PXe+SuwATFnvEgJLFOR2Cffu1x97NPG4DyaEHvbLXE8pokxwZS5g9r2QXfCbHyMJGfc/a/StXc7rL7FCX/750LKm02QOFkSaDX/zN7U8UON3pYkH3pw5LuebKajZGmr2THd31vObUTrD8rDrKKrjQUyetw8qCQmBi+C+1GUyCgsVU5Q+QLd3XsmypsM/A6CruomlHCKI9CqhdLCJGniNt3QwoNgo4l+O6qyPijPHohIGNwUIqFZT3ExBGC6v4vWQL5o5Bo8QxycxbpZldiDSD3Y9kq+mg9QdTiYpspa97+CtXWhBgnB+sq5dPycl1xxOoYoTaen07+tM/GGmoLu8+W8goKxuWnAPlxPLqY+mdEsYgRfRqzAW8i3QJH80eMiom4TE1SmbsZgNS9M7sstiYYqTf7/pFxtC+4++g6vDpYGCih2K02WBJk6Fxz0MNEglxyK0SKpaBn1aRwsedhrBg1TK3AkTemgIXHvXKlXnTPXiP3hB8ri2MnxxG/a36EpPGteJV5L15ltaMBo552pYWl0e+IkSwF58qy/Bppdav2Zr8Uy5apzLZIZjX6SmVCgtKAZPFCorCY4qK9RoDOvyjApMpulW8AP0RQneb6XPvqQsHE3iQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>", new CultureInfo("en"));
+
+            var updaterUi = new UpdaterUI(manager, SynchronizationContext.Current) {UseHiddenSearch = true};
+            updaterUi.ShowUserInterface();
+
         }
 
         private void InitializeComponent()
@@ -49,24 +44,24 @@ namespace MovieGateway
                 Icon = Resources.TrayIcon
             };
 
-            _listenMenuItem = new ToolStripMenuItem { CheckOnClick = true, Checked = true };
-            _listenMenuItem.Text = "Check for Links";
+            _listenMenuItem = new ToolStripMenuItem
+            {
+                CheckOnClick = true,
+                Checked = true,
+                Text = "Check for Links"
+            };
 
-            _autoStartMenuItem = new ToolStripMenuItem{CheckOnClick = true, Checked = false};
-            _autoStartMenuItem.Text = "Start with Windows";
+            _autoStartMenuItem = new ToolStripMenuItem
+            {
+                CheckOnClick = true,
+                Checked = false,
+                Text = "Start with Windows"
+            };
             _autoStartMenuItem.CheckedChanged += _autoStartMenuItem_CheckedChanged;
 
-            if (IsStartupItem())
-            {
-                _autoStartMenuItem.Checked = true;
-            }
-            else
-            {
-                _autoStartMenuItem.Checked = false;
-            }
+            _autoStartMenuItem.Checked = IsStartupItem();
 
-            _closeMenuItem = new ToolStripMenuItem();
-            _closeMenuItem.Text = "Close";
+            _closeMenuItem = new ToolStripMenuItem {Text = "Close"};
             _closeMenuItem.Click += CloseMenuItem_Click;
 
             _trayIconContextMenu = new ContextMenuStrip();
@@ -93,14 +88,23 @@ namespace MovieGateway
 
                     if (clipboardtext.StartsWith("http://vivo.sx/"))
                     {
-                        _trayIcon.ShowBalloonTip(50, "Found a viable Link", "Going to open: " + clipboardtext, ToolTipIcon.Info);
+                        _trayIcon.ShowBalloonTip(50, "Found a viable Link", "Going to open: " + clipboardtext,
+                            ToolTipIcon.Info);
                         Process.Start(await GetVivoDirectlink(clipboardtext));
                     }
 
                     if (clipboardtext.StartsWith("http://streamcloud.eu"))
                     {
-                        _trayIcon.ShowBalloonTip(50, "Found a viable Link", "Going to open: " + clipboardtext, ToolTipIcon.Info);
+                        _trayIcon.ShowBalloonTip(50, "Found a viable Link", "Going to open: " + clipboardtext,
+                            ToolTipIcon.Info);
                         Process.Start(await GetStreamcloudDirectlink(clipboardtext));
+                    }
+
+                    if (clipboardtext.StartsWith("http://shared.sx"))
+                    {
+                        _trayIcon.ShowBalloonTip(50, "Found a viable Link", "Going to open: " + clipboardtext,
+                            ToolTipIcon.Info);
+                        Process.Start(await GetSharedDirectlink(clipboardtext));
                     }
                 }
             }
@@ -114,16 +118,47 @@ namespace MovieGateway
 
         private void CloseMenuItem_Click(object sender, EventArgs e)
         {
-            if (
-                MessageBox.Show("Do you really want to close me?", "Are you sure?", MessageBoxButtons.YesNo,
-                    MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            if (MessageBox.Show("Do you really want to close me?", "Are you sure?", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2) != DialogResult.Yes) return;
+            ClipboardNotification.ClipboardUpdate -= ClipboardNotification_ClipboardUpdate;
+            Application.Exit();
+        }
+
+
+        private async Task<string> GetSharedDirectlink(string sharedLink)
+        {
+            using (HttpClient client = new HttpClient())
             {
-                ClipboardNotification.ClipboardUpdate -= ClipboardNotification_ClipboardUpdate;
-                Application.Exit();
+                string sharedSource = await client.GetStringAsync(sharedLink);
+
+                var regexHash = new Regex("\"hash\" value=\"(.*?)\"");
+                var regexExpires = new Regex("\"expires\" value=\"(.*?)\"");
+                var regexTimestamp = new Regex("\"timestamp\" value=\"(.*?)\"");
+                Match matchHash = regexHash.Match(sharedSource); //matchVivo.Groups[1].Value)
+                Match matchExpires = regexExpires.Match(sharedSource); //matchExpires.Groups[1].Value)
+                Match matchTimestamp = regexTimestamp.Match(sharedSource); //matchTimestamp.Groups[1].Value)
+                string hash = matchHash.Groups[1].Value;
+                string expires = matchExpires.Groups[1].Value;
+                string timestamp = matchTimestamp.Groups[1].Value;
+                var formContent = new FormUrlEncodedContent(new[]
+                    {
+                        new KeyValuePair<string, string>("hash", hash),
+                        new KeyValuePair<string, string>("expires", expires),
+                        new KeyValuePair<string, string>("timestamp", timestamp),
+                    });
+                await Task.Run(() =>
+                {
+                    Thread.Sleep(100);
+                });
+                var response = await client.PostAsync(sharedLink, formContent);
+                var stringContent = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("data-url=\"(.*?)\"");
+                Match match = regex.Match(stringContent);
+                return match.Groups[1].Value;
             }
         }
 
-        private async Task<string> GetStreamcloudDirectlink(string StreamcloudLink)
+        private async Task<string> GetStreamcloudDirectlink(string streamcloudLink)
         {
             using (HttpClient client = new HttpClient())
             {
@@ -131,13 +166,13 @@ namespace MovieGateway
                 {
                     new KeyValuePair<string, string>("op", "download2"),
                     new KeyValuePair<string, string>("usr ", "login"),
-                    new KeyValuePair<string, string>("id", StreamcloudLink.Split('/')[3]),
-                    new KeyValuePair<string, string>("fname", StreamcloudLink.Split('/')[4]),
+                    new KeyValuePair<string, string>("id", streamcloudLink.Split('/')[3]),
+                    new KeyValuePair<string, string>("fname", streamcloudLink.Split('/')[4]),
                     new KeyValuePair<string, string>("referer", ""),
                     new KeyValuePair<string, string>("hash", ""),
                     new KeyValuePair<string, string>("iamhuman", "Weiter zum Video")
                 });
-                var response = await client.PostAsync(StreamcloudLink, formContent);
+                var response = await client.PostAsync(streamcloudLink, formContent);
                 var stringContent = await response.Content.ReadAsStringAsync();
                 var regex = new Regex("file: \"(.*?)\",");
                 Match match = regex.Match(stringContent);
@@ -145,11 +180,11 @@ namespace MovieGateway
             }
 
         }
-        private async Task<string> GetVivoDirectlink(string VivoLink)
+        private async Task<string> GetVivoDirectlink(string vivoLink)
         {
             using (HttpClient client = new HttpClient())
             {
-                string vivoSource = await client.GetStringAsync(VivoLink);
+                string vivoSource = await client.GetStringAsync(vivoLink);
                 var regexHash = new Regex("\"hash\" value=\"(.*?)\"");
                 var regexExpires = new Regex("\"expires\" value=\"(.*?)\"");
                 var regexTimestamp = new Regex("\"timestamp\" value=\"(.*?)\"");
@@ -166,7 +201,7 @@ namespace MovieGateway
                         new KeyValuePair<string, string>("timestamp", timestamp),
                     });
 
-                var response = await client.PostAsync(VivoLink, formContent);
+                var response = await client.PostAsync(vivoLink, formContent);
                 var stringContent = await response.Content.ReadAsStringAsync();
                 var regex = new Regex("data-url=\"(.*?)\"");
                 Match match = regex.Match(stringContent);
@@ -178,7 +213,7 @@ namespace MovieGateway
         private bool IsStartupItem()
         {
             RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-            if (rkApp.GetValue("MovieGateway") == null) return false;
+            if (rkApp != null && rkApp.GetValue("MovieGateway") == null) return false;
             return true;
         }
 
@@ -190,14 +225,14 @@ namespace MovieGateway
                 RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
 
                 if (!IsStartupItem())
-                    rkApp.SetValue("MovieGateway", Application.ExecutablePath);
+                    if (rkApp != null) rkApp.SetValue("MovieGateway", Application.ExecutablePath);
             }
             if (_autoStartMenuItem.Checked == false)
             {
                 RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
                 if (IsStartupItem())
                 {
-                    rkApp.DeleteValue("MovieGateway", false);
+                    if (rkApp != null) rkApp.DeleteValue("MovieGateway", false);
                 }
 
             }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,10 +8,16 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
 namespace MovieGateway.Properties {
-    using System;
-    
-    
     /// <summary>
     ///   Eine stark typisierte Ressourcenklasse zum Suchen von lokalisierten Zeichenfolgen usw.
     /// </summary>
@@ -19,27 +25,27 @@ namespace MovieGateway.Properties {
     // -Klasse über ein Tool wie ResGen oder Visual Studio automatisch generiert.
     // Um einen Member hinzuzufügen oder zu entfernen, bearbeiten Sie die .ResX-Datei und führen dann ResGen
     // mit der /str-Option erneut aus, oder Sie erstellen Ihr VS-Projekt neu.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [DebuggerNonUserCode()]
+    [CompilerGenerated()]
     internal class Resources {
         
-        private static global::System.Resources.ResourceManager resourceMan;
+        private static ResourceManager resourceMan;
         
-        private static global::System.Globalization.CultureInfo resourceCulture;
+        private static CultureInfo resourceCulture;
         
-        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal Resources() {
         }
         
         /// <summary>
         ///   Gibt die zwischengespeicherte ResourceManager-Instanz zurück, die von dieser Klasse verwendet wird.
         /// </summary>
-        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static ResourceManager ResourceManager {
             get {
-                if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MovieGateway.Properties.Resources", typeof(Resources).Assembly);
+                if (ReferenceEquals(resourceMan, null)) {
+                    ResourceManager temp = new ResourceManager("MovieGateway.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -50,8 +56,8 @@ namespace MovieGateway.Properties {
         ///   Überschreibt die CurrentUICulture-Eigenschaft des aktuellen Threads für alle
         ///   Ressourcenzuordnungen, die diese stark typisierte Ressourcenklasse verwenden.
         /// </summary>
-        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,10 +69,10 @@ namespace MovieGateway.Properties {
         /// <summary>
         ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Icon ähnlich wie (Symbol).
         /// </summary>
-        internal static System.Drawing.Icon TrayIcon {
+        internal static Icon TrayIcon {
             get {
                 object obj = ResourceManager.GetObject("TrayIcon", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
+                return ((Icon)(obj));
             }
         }
     }
